7ebbcdc2b336ecf88cf939ce5415f9114e11b8c6
AlessandroSaba1991 <sabaalessandro515@gmail.com>
2022-05-03 16:04:05 +0200

    add header site

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,35 @@
 <template>
   <div id="app">
-    
+    <SiteHeader />
   </div>
 </template>
 
 <script>
-
+import SiteHeader from './components/HeaderComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    SiteHeader
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  background-color: black;
+@import url('https://fonts.googleapis.com/css2?family=Fira+Sans+Condensed:wght@300;400;500;700&display=swap');
+@import '@/assets/sass/style';
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+img{
+  max-width: 100%;
+}
+
+#app{
+  font-family: 'Fira Sans Condensed', sans-serif;
 }
 </style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,72 @@
+<template>
+  <header>
+    <div class="container">
+      <div class="row align_center">
+        <div class="col_1">
+          <img src="@/assets/img/dc-logo.png" alt="" />
+        </div>
+        <div class="col_7 ms-auto">
+          <nav>
+            <ul>
+              <li v-for="(title, index) in navMenu" :key="index">
+                <a href="#">{{ title }}</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+  data() {
+    return {
+      navMenu: [
+        "Characters",
+        "Comics",
+        "Movies",
+        "Tv",
+        "Games",
+        "Collectables",
+        "Videos",
+        "Fans",
+        "News",
+        "Shop",
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/sass/partials/variables";
+header {
+  padding: 1rem;
+  ul {
+    list-style: none;
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    li {
+      padding: 0 1rem;
+
+      a {
+        text-decoration: none;
+        text-transform: uppercase;
+        font-size: 14px;
+        font-weight: 400;
+        padding: 2rem 0;
+        color: black;
+      }
+
+      &:nth-child(2) a {
+        color: $primaryColor;
+        box-shadow: 0px 8px $primaryColor;
+      }
+    }
+  }
+}
+</style>
